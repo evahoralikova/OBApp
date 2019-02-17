80ccfae620f816c3c66225a08a261c3ab780e283
Eva Horalikova <eva.horalikova@gmail.com>
2019-02-17 15:45:32 +0100

    First version of bottom navigation menu with one menu item. wip

--- MyOB/app/src/main/java/com/example/evaho/myob/MainActivity.java
+++ MyOB/app/src/main/java/com/example/evaho/myob/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.evaho.myob;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -18,10 +23,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txt_header1=(TextView) findViewById(R.id.textView_header1);
+        /*txt_header1=(TextView) findViewById(R.id.textView_header1);
         txt_content1=(TextView) findViewById(R.id.textView_content1);
         txt_header2=(TextView) findViewById(R.id.textView_header2);
-        txt_content2=(TextView) findViewById(R.id.textView_content2);
+        txt_content2=(TextView) findViewById(R.id.textView_content2); */
         bt_denik = (Button) findViewById(R.id.bt_denik);
         bt_prehled = (Button) findViewById(R.id.bt_prehled);
 
@@ -40,6 +45,30 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener
+                (new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment selectedFragment = null;
+                        switch (item.getItemId()) {
+                            case R.id.action_item1:
+                                selectedFragment = ItemOneFragment.newInstance();
+                                break;
+                        }
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.linear_layout, selectedFragment);
+                        transaction.commit();
+                        return true;
+                    }
+                });
+
+        //Manually displaying the first fragment - one time only
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.linear_layout, ItemOneFragment.newInstance());
+        transaction.commit();
 
     }
 }
