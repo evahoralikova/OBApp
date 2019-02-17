c9ded53002303ec8889cb5f5882a51df3c689727
Eva Horalikova <eva.horalikova@gmail.com>
2019-02-17 14:01:50 +0100

    Added button to start activity denik.

--- MyOB/app/src/main/java/com/example/evaho/myob/MainActivity.java
+++ MyOB/app/src/main/java/com/example/evaho/myob/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.evaho.myob;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button bt_denik;
     TextView txt_header1;
     TextView txt_content1;
     TextView txt_header2;
@@ -17,5 +21,14 @@ public class MainActivity extends AppCompatActivity {
         txt_content1=(TextView) findViewById(R.id.textView_content1);
         txt_header2=(TextView) findViewById(R.id.textView_header2);
         txt_content2=(TextView) findViewById(R.id.textView_content2);
+        bt_denik = (Button) findViewById(R.id.bt_denik);
+
+        bt_denik.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent  = new Intent (v.getContext(), Main2Activity_denik.class);
+                startActivity(intent);
+            }
+        });
     }
 }
