095018500e0d50f48c57676b511ae17a6af46bf2
Eva Horalikova <eva.horalikova@gmail.com>
2019-03-31 16:51:25 +0200

    Added a url to ORIS

--- MyOB/app/src/main/java/com/example/evaho/myob/OverviewFragment.java
+++ MyOB/app/src/main/java/com/example/evaho/myob/OverviewFragment.java
@@ -3,9 +3,11 @@ package com.example.evaho.myob;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class OverviewFragment extends Fragment {
     public static OverviewFragment newInstance() {
@@ -16,6 +18,7 @@ public class OverviewFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
